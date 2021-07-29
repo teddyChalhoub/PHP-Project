d7c6df1b027e3b893abc2665d7547b9354b97d23
teddyChalhoub <teddychalhoub0@gmail.com>
2021-07-29 09:00:47 +0300

    added security to connection

--- src/Classes/Connection.php
+++ src/Classes/Connection.php
@@ -4,17 +4,22 @@ namespace App\Classes;
 
 use PDO;
 use PDOException;
+use Dotenv\Dotenv;
 
 class Connection{
 
   public function connect(){
     
-    $servername = "localhost";
-    $username = "admin";
-    $password = "tyt1eor2dua3";
-    $db = "PHPDB";
+    $dotEnv = Dotenv::createImmutable(dirname(__DIR__,2));
+    $dotEnv->load();
+    
+    $servername = $_ENV["DB_SERVER"];
+    $username = $_ENV["DB_USER"];
+    $password = $_ENV["DB_PASS"];
+    $db = $_ENV["DB_NAME"];
     
     try {
+
       $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
       // set the PDO error mode to exception
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
